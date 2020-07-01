dacfe6a6dbe8680cb3a36e12b0204fa3df941b5c
Ezekiel <iamwebwiz@gmail.com>
2020-07-01 08:38:03 +0100

    Fetching data

--- src/index.js
+++ src/index.js
@@ -49,7 +49,13 @@ const NotHiring = () => (
 )
 
 class Library extends Component {
-  state = { open: true, freeBookmark: true, hiring: true }
+  state = {
+    open: true,
+    freeBookmark: true,
+    hiring: true,
+    data: [],
+    loading: false,
+  }
 
   toggleOpenClosed = () => {
     this.setState(previousState => ({
@@ -57,13 +63,40 @@ class Library extends Component {
     }))
   }
 
+  componentDidMount() {
+    this.setState({ loading: true })
+
+    fetch('http://hplussport.com/api/products/order/price/sort/asc/qty/1')
+      .then(data => data.json())
+      .then(data => {
+        this.setState({ data, loading: false })
+      })
+  }
+
+  componentDidUpdate() {
+    console.log('The component has been updated')
+  }
+
   render() {
     const { books } = this.props
-    const { freeBookmark, hiring } = this.state
+    const { freeBookmark, hiring, loading, data } = this.state
 
     return (
       <div>
         {hiring ? <Hiring /> : <NotHiring />}
+        {loading ? (
+          'Loading...'
+        ) : (
+          <div>
+            {data.map(product => (
+              <div key={product.id}>
+                <h3>Library Product of the Week!</h3>
+                <h4>{product.name}</h4>
+                <img alt={product.name} src={product.image} height={100} />
+              </div>
+            ))}
+          </div>
+        )}
         <h2>The library is {this.state.open ? 'open' : 'closed'}</h2>
         <button onClick={this.toggleOpenClosed}>Change</button>
 
